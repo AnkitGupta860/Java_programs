ccf6a8c177c4108f40752f26922f84c44e1ccda4
Ankit Gupta <ankit.gupta_cs.aiml20@gla.ac.in>
2021-09-04 10:32:08 +0530

    practice

---
+++ src/_13_Question8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class _13_Question8 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String num = obj.next();
+        int k = num.length();
+        for(int i=0;i<k-1;i++){
+            continue;
+        }
+
+    }
+}
+
